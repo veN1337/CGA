a1d5c7e6768e0663d8e1b7269d1fd0bf9ff3d8ca
Xenowar <Xenowar@139.6.214.7>
2012-10-18 09:30:44 +0200

    HansInitiate

---
+++ CGA3/src/org/amcgala/example/turtle/TurtleGraph.java
@@ -0,0 +1,18 @@
+package org.amcgala.example.turtle;
+
+import org.amcgala.TurtleMode;
+
+public class TurtleGraph extends TurtleMode{
+
+	public static void main(String[] args) {
+		new TurtleGraph();
+        
+    }
+	
+	@Override
+	public void turtleCommands() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
