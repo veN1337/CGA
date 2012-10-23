077578ae93fcf01ef0f12c0a9d898063d95df86d
Xenowar <Xenowar@139.6.214.58>
2012-10-23 13:23:45 +0200

    Graph voll toll

--- CGA3/src/Graphen/Graph.java
+++ CGA3/src/Graphen/Graph.java
@@ -1,5 +1,7 @@
 package Graphen;
 
+import java.awt.Color;
+
 import org.amcgala.framework.renderer.Renderer;
 import org.amcgala.framework.shape.AbstractShape;
 import org.amcgala.framework.shape.Line;
@@ -11,8 +13,8 @@ import org.amcgala.framework.shape.shape2d.Text;
  */
 public abstract class Graph extends AbstractShape {
 	
-	int x =5;
-	int y =5;
+	int x =30;
+	int y =20;
 
     private Line line1 = new Line(-390, 0, 390, 0);
     private Line line2 = new Line(0, -270, 0, 270);
@@ -20,11 +22,17 @@ public abstract class Graph extends AbstractShape {
 
     @Override
     public void render(Renderer renderer) {
+    	line1.render(renderer);
+        line2.render(renderer);
+        double sl=0.1;
+        for(double x1=-x; x1<x; x1+=sl){
+        	Line graph = new Line(x1*(390/x),f(x1)*(270/y),(x1+sl)*(390/x),f(x1+sl)*(270/y));
+        	graph.setColor(Color.RED);
+        	graph.render(renderer);
+        }
     	for(int i = -5; i <=5;i++){
     		if(i<0){
     			new Line(i*(780/11),0,i*(780/11),-6).render(renderer);
-    			double test =((double)x*((double)i/(double)x));
-    			//new Text(""+test,i*(780/11),-8).render(renderer);
     		}
     		else{
     			new Line(i*(780/11),0,i*(780/11),6).render(renderer);
@@ -38,9 +46,32 @@ public abstract class Graph extends AbstractShape {
     			new Line(0,i*(540/11),-6,i*(540/11)).render(renderer);
     		}
     	}
-        line1.render(renderer);
-        line2.render(renderer);
+    	for(int i = -5; i <=5;i++){
+    		if(i<0){
+    			double test =(((double)x/5)*i);
+    			new Text(""+test,i*(780/11)-40,-25).render(renderer);
+    		}
+    		else if(i>0){
+    			double test =(((double)x/5)*i);
+    			new Text(""+test,i*(780/11)-20,8).render(renderer);
+    		}
+    	}
+    	for(int i = -5; i <=5;i++){
+    		if(i>0){
+    			double test =(((double)y/5)*i);
+    			new Text(""+test,10,i*(540/11)-8).render(renderer);
+    		}
+    		else if (i<0){
+    			double test =(((double)y/5)*i);
+    			new Text(""+test,-68,i*(540/11)-10).render(renderer);
+    		}
+    	}
+        
         //text.render(renderer);
+    }
+    
+    public double f(double x){
+    	return x*x;
     }
 
     /**
