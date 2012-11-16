fce9bbe9769dc2156d416e9b350f68800aa35065
veN1337 <sascha.rothkopf@arcor.de>
2012-11-16 14:13:23 +0100

    Linie eingebunden

--- CGA6/src/Input.java
+++ CGA6/src/Input.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.common.eventbus.Subscribe;
 import org.amcgala.Amcgala;
 import org.amcgala.Scene;
 import org.amcgala.framework.event.InputHandler;
 import org.amcgala.framework.event.MouseClickedEvent;
 import org.amcgala.framework.scenegraph.Node;
+import org.amcgala.framework.shape.Line;
 import org.amcgala.framework.shape.shape2d.Rectangle;
 
 /**
@@ -12,6 +16,11 @@ import org.amcgala.framework.shape.shape2d.Rectangle;
 public class Input extends Amcgala implements InputHandler {
     private Scene inputScene = new Scene("inputScene");
     private Node inputNode = new Node("input");
+    
+    private int prevX = 0;
+    private int prevY = 0;
+    
+    private List<Rectangle> liste = new ArrayList<Rectangle>();
     
     int boxsize = 300;
     
@@ -23,7 +32,20 @@ public class Input extends Amcgala implements InputHandler {
 
     @Subscribe
     public void addPoint(MouseClickedEvent e) {
-        inputScene.add(new Rectangle(e.getX(), e.getY(), 5, 5), inputNode);
+    	if(liste.size() >= 2) {
+    		inputScene.removeShape(liste.get(0).getLabel());
+    		inputScene.removeShape(liste.get(1).getLabel());
+    		inputScene.removeShape("linie");
+       		liste.clear();
+    	}
+    	Rectangle r = new Rectangle(e.getX(), e.getY(), 6, 6);
+    	liste.add(r);
+        inputScene.add(r, inputNode);
+        if(liste.size() == 2) {
+        	Line l = new Line(prevX, prevY, e.getX() + 3, e.getY() + 3, "linie");
+        	inputScene.add(l, inputNode);
+        }
+        prevX = e.getX() + 3; prevY = e.getY() + 3;
     }
 
     public static void main(String[] args) {
